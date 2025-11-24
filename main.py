2d7f04d125de2eaae6d3f5d632af727c32e0c48e
Erika Valle-Baird <68307227+Airikuh@users.noreply.github.com>
2025-11-24 04:15:56 -0800

    Add files via upload
    File to hold the data produced and create visualizations based on metrics

---
+++ main.py
@@ -0,0 +1,17 @@
+# File to Input Likert Scale Data for Software/Metrics Visualizations (Graphs)
+
+
+# Array/List of Types of LLM Output Categories (High-level Summary, Use cases, etc)
+
+
+
+# Array/List of each of the Data Set Categories holding the names of the text files as the items
+
+
+
+# Array that Holds Likert Scale Categories
+
+
+
+
+# Arrays/List that Hold Likert Scale Rating
